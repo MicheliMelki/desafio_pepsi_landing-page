9206534644505ffc4981531df6b0da9a382c6eca
Micheli <michelimelki@gmail.com>
2025-10-24 00:20:25 -0300

    Adicionando funcionalidades com JS

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+// ALTERAÇÃO DE IMAGENS NA PÁGINA
+function imgSlider(anything) {
+  document.querySelector(".pepsi").src = anything;
+}
+// MUDANÇA DE COR DE FUNDO
+function chanceBgColor(color) {
+  const sec = document.querySelector(".sec");
+  sec.style.background = color;
+}
